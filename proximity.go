93f410afcbad4ce97a87ede36cc3e9ac4deb2fb2
Benjamin Rokseth <bensinober@gmail.com>
2016-11-22 08:48:10 +0100

    Setup tlsConfig and fix headers

--- proximity.go
+++ proximity.go
@@ -9,15 +9,18 @@ import (
 	"net/url"
 	"net/http"
 	"os"
-	//"strings"
+	"time"
+	"log"
 	"io/ioutil"
-
+	metrics "github.com/rcrowley/go-metrics"
 )
 
 var (
 	matchid = uint64(0)
 	connid  = uint64(0)
-
+	metricsInterval = flag.Int("m", 5, "Interval of metrics logging")
+	certFile    = flag.String("cert", "cert.pem", "A PEM eoncoded certificate file.")
+	keyFile     = flag.String("key", "key.pem", "A PEM encoded private key file.")
 	localAddr   = flag.String("l", ":9999", "local address")
 	remoteAddr  = flag.String("r", "http://localhost:80", "remote address")
 	onlyHeaders = flag.Bool("h", false, "dump only headers")
@@ -30,10 +33,13 @@ func main() {
 	flag.Parse()
 
 	fmt.Printf("Proxying from %v to %v\n\n", *localAddr, *remoteAddr)
+	go metrics.Log(metrics.DefaultRegistry, time.Duration(*metricsInterval)*time.Second, log.New(os.Stderr, "", 0))
 	serve()
 }
 
 func serve() {
+	requestsCounter := metrics.GetOrRegisterCounter("numRequests", nil)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 
 		end, err := url.Parse(req.URL.RequestURI())
@@ -50,17 +56,6 @@ func serve() {
 			w.WriteHeader(599)
 			return
 		}
-		// copy host + scheme to response
-		end.Host = target.Host
-		end.Scheme = target.Scheme
-		
-		// build request and copy headers
-		b2b, err := http.NewRequest(req.Method, end.String(), req.Body)
-		for header, values := range req.Header {
-			for _, value := range values {
-				b2b.Header.Add(header, value)
-			}
-		}
 
 		fmt.Printf("----- PARAMS ------\n")
 		args, _ := url.ParseQuery(req.URL.RawQuery)
@@ -68,13 +63,48 @@ func serve() {
 			fmt.Fprintf(os.Stderr, "%s:\t%s\t\n", a, v[0])
 		}
 
+		// copy host + scheme to response
+		end.Host = target.Host
+		end.Scheme = target.Scheme
+
+		// Setup tls transport
+		var tlsConfig
+
+		if *noverify {
+			tlsConfig = &tls.Config{ InsecureSkipVerify: true }
+		} else {
+			// Load client cert
+			cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			tlsConfig = &tls.Config{
+				Certificates: []tls.Certificate{cert},
+			}
+			tlsConfig.BuildNameToCertificate()
+		}
+		transport := &http.Transport{TLSClientConfig: tlsConfig}
+
+		// build destination request and copy headers
+		newreq, err := http.NewRequest(req.Method, end.String(), req.Body)
+		
+		for header, values := range req.Header {
+			for _, value := range values {
+				w.Header().Add(header, value)
+			}
+		}
+
 		// dump request
+		requestsCounter.Inc(1)
 		fmt.Printf("----- REQUEST ------\n")
-		if x, err := httputil.DumpRequestOut(b2b, !*onlyHeaders); err == nil {
+		if x, err := httputil.DumpRequestOut(newreq, !*onlyHeaders); err == nil {
 			fmt.Fprintf(os.Stderr, "%s\n", string(x))
 		}
 
+		// Do real request
 		client := &http.Client{
+			Transport: transport,
 			CheckRedirect: func(req *http.Request, via []*http.Request) error {
 				if len(via) > 10 {
 					return fmt.Errorf("stopped after 10 redirects")
@@ -91,17 +121,13 @@ func serve() {
 				}
 				return nil
 			},
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: *noverify,
-				},
-			},
 		}
 
 		// build response
 		var res *http.Response
-		res, err = client.Do(b2b)
+		res, err = client.Do(newreq)
 		defer res.Body.Close()
+
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "failed to read response: %s\n", err)
@@ -111,8 +137,9 @@ func serve() {
 
 		// Dump response - optionally with body
 		fmt.Printf("----- RESPONSE ------\n")
+
 		if x, err := httputil.DumpResponse(res, !*onlyHeaders); err == nil {
-			fmt.Fprintf(os.Stderr, "%s\n", string(x))
+			fmt.Printf(string(x))
 		}
 
 		// let response through
